f83ca615b4a3f22677b518fea0747715d37837d1
Liam Turnbull <ghostboy21134@gmail.com>
2021-11-09 01:01:50 +0000

    put weather data in the 1d arrays of city1/2 modified a for loop to properly loop for the EC

--- main.cpp
+++ main.cpp
@@ -3,17 +3,11 @@
 //Date:11/8/2021
 
 //A program to practice working with multi-dimensional arrays
-
+#include <utility>
 #include <iostream>
 #include <string>
-
-
-
-
-
+#include "Input_Validation_Extended.h"
 using namespace std;
-
-
 
 //functions protos
 void display_city(int i); // this one takes i and does simple branching path for determining whether to say Cleburne or fort worth inside the for loops
@@ -23,55 +17,41 @@ int main()
   const int CITY = 2;
   const int WEEK = 7;
   int temperature[CITY][WEEK];
-
+  int city1[7]= {59,49,58,63,69,75,74}; //cleburne
+  int city2[7]= {56,48,58,63,69,76,71}; //ft worth
+  //storing 7 variables 0-6
+  
   //Note your input data from the above
-  cout << "Enter all temperature for a week of Cleburne and then Fort Worth. \n";
   // Inserting the values into the temperature array
   // note for every dimension of the array you need a for loop.
   // 2 dimensions = 2 for loops
+  
+
+cout <<"########################" << endl;
+cout << "      CITY, Day # Temp"<<endl;
+cout <<"########################" << endl;
+
   for (int i = 0; i < CITY; ++i)
   {
     for(int j = 0; j < WEEK; ++j)
     {
-      display_city(i);
-      cout << ", Day " << j + 1 << " : ";
-      //floats break this cin >> temperature[i][j];
-
-
-
+      temperature[i][j] = city2[j];
+      display_city(i); cout << ", Day " << j+1 <<"  " << temperature[i][j] << endl;
     }
   }
-  cout << "\n\nDisplaying Values:\n";
-  // Accessing the values from the temperature array
-  for (int i = 0; i < CITY; ++i)
-  {
-    for(int j = 0; j < WEEK; ++j)
-    {
-      display_city(i);
-      cout << ", Day " << j + 1 
-      << " = " << temperature[i][j] << endl;
-    }
-  }
+
   return 0;
 }
-
-//For up to 5 Points Extra Credit
-//Expand this program:
-//1. Create a one dimensional array, cityOne[7] to store the 7 temperatures of city 1, assume that value 0 represents day 1, 1 represents day 2....
-//2. Create a one dimensional array, cityTwo[7] to store the 7 temperatures of city 2 , assume that value 0 represents day 1, 1 represents day 2....
-//3. Using a for loop (or nested for loop), feed the data of CityOne[] and CityTwo[] into temperature[i][j], where i is index of the City Number, and j is the index of the temp for the day.
-//4. print the results of temperature[i][j]; to the screen.
-
 void display_city(int i)
 // takes i and gets the value of one or 0 to determine whether to out put cleburne or fort worth does not have a endl inside
 {
   if(i == 0)
   {
-    cout << "Cleburne ";
+    cout << "  Cleburne";
   }
   else if (i==1)
   {
-    cout << "Fort Worth ";
+    cout << "Fort Worth";
   }
 }
 
